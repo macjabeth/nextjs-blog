762ce320f866dc4f7d5c111a48b6bacaa4a21d7c
Jonathan Picazo <macbethjonathan@gmail.com>
2021-08-01 23:26:35 -0500

    📦 NEW: Pre-rendering and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -12,6 +13,29 @@ export default function Home() {
         <p>Hi, I'm <strong>Jonathan</strong> 👋 I'm a software engineer at
         Northwestern Mutual and this is a sample site built using Next.js!</p>
       </section>
+      <section className='text-xl leading-6 mt-4'>
+        <h2 className='text-2xl font-semibold leading-6 my-4'>Blog</h2>
+        <ul className='space-y-4'>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
